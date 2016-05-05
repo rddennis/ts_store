145ba35d0b218c1a96b18dbed052756786bc78d1
rddennis <ronesha_dennis@yahoo.com>
2016-05-05 11:00:57 -0500

    adds validations for products, brands, and categories

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,4 @@
 class Brand < ActiveRecord::Base
   has_many :products
+  validates_uniqeness_of :name
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,10 @@ class Product < ActiveRecord::Base
 
   mount_uploader :avatar, AvatarUploader
 
+  validates_presence_of :name, :price, :quantity, :description, :brand, :category
+  validates_numericality_of :price, greater_than_or_equal_to: 0.01
+  validates_numericality_of :quantity, greater_than_or_equal_to: 0
+
   # validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   # # allows us to only accept file types that are images
 
