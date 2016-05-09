6bfe27acaf1f86074f09309296b3140d260228ae
rddennis <ronesha_dennis@yahoo.com>
2016-05-09 10:26:55 -0500

    Adds search form for store products

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,4 +11,7 @@ class Product < ActiveRecord::Base
   # validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   # # allows us to only accept file types that are images
 
+def self.search_by_name_or_description(string)
+  where("name LIKE ? OR description LIKE ?", "%#{string}%", "%#{string}%")
+end
 end
