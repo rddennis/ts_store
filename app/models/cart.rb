1f1b534cf688db59ee3c0a2377864280ed5141cf
rddennis <ronesha_dennis@yahoo.com>
2016-05-10 10:11:12 -0500

    moves destroy CRUD method from model to controller and includes current cart in Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,21 +1,12 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
 
-  def destroy
-    if cart.destroy
-      session[:card_id] = nil
-      flash[:notice] = "Cart was successfully deleted."
-      redirect_to shop_path
-    else
-      flash.now[:alert] = "Could not delete cart."
-      redirect_to edit_cart_path
-    end
-  end
+def subtotal
+  #SQL version of summing, which is faster than Ruby
+  line_items.select("SUM(quantity * price) AS sum")[0].sum
 
-  def subtotal
-    #SQL version of summing, which is faster than Ruby
-    line_items.select("SUM(quantity * price) AS sum")[0].sum
+  #RUBY version of summing
+  # line_items.to_a.sum{ |item| item.total }
+end
 
-    #RUBY version of summing
-    # line_items.to_a.sum{ |item| item.total }
 end
