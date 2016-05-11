b3a1f0662d59022cd468be553f8bb198b285db79
rddennis <ronesha_dennis@yahoo.com>
2016-05-11 08:31:32 -0500

    updates cart to adjust quantity when new items are added

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -5,7 +5,7 @@ before_action :set_cart
 
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product, price: product.price)
+    @line_item = @cart.add_product(product.id)
 
     if @line_item.save
       flash[:notice] = "#{product.name} was successfully added to your cart!"
@@ -14,6 +14,33 @@ before_action :set_cart
       flash.now[:alert] = "We could not add this item to your cart."
       redirect_to :back
     end
+  end
+
+  def destroy
+    @line_item = @cart.line_items.find(params[:id])
+    if @line_item.destroy
+      redirect_to edit_cart_path(@cart), notice: "Item removed from cart."
+    else
+      redirect_to @cart, alert: "Could not remove item from cart."
+    end
+  end
+
+  def update
+    @line_item = @cart.line_items.find(params[:id])
+    if @line_item.quantity <= 0
+      @line_item.destroy
+      redirect_to edit_cart_path(@cart), notice: "Item removed from cart."
+    elsif @line_item.update(line_items_params)
+      redirect_to edit_cart_path(@cart), notice: "#{@line_item.product.name} quantity updated."
+    else
+      redirect_to edit_cart_path(@cart), alert: "Could not update quantity."
+    end
+  end
+
+  private
+
+  def line_items_params
+    params.require(:line_item).permit(:quantity)
   end
 
 end
